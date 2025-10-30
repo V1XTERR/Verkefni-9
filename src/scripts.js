6f683b49794063e73e61c7de121051beab88dd12
Viktor Blær <Hafthorsson14@gmail.com>
2025-10-30 22:42:28 +0000

    fyrsta og seinasta pushið, of mikið stærfræði í vikunni...

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,48 @@
 // TODO leit í API
 // TODO Sýna niðurstöður á síðunni
+import  { el, empty }  from "../src/lib/elements.js";
+
+
+const artSearcher = document.querySelector('.art-searcher');
+
+const statusElement = el('p', {}, "Sláðu inn leitarorð til að byrja")
+
+const resultsElement = el('ul')
+const resultElement = el('div', {}, statusElement, resultsElement) 
+
+
+const inputElement = document.createElement
+('input')
+inputElement.setAttribute('name', 'query')
+
+/**
+ *
+ *  @param {SubmitEvent} e  Form submit event.
+ * 
+ */
+
+
+function onSubmitHandler(e) {
+    e.preventDefault();
+    empty(resultsElement);
+
+
+
+    const query = inputElement.value;
+
+    statusElement.textContent = `Leitað að: ${query}...`;
+
+    const resultitems = el('li', {}, "Leitaði að" + query)
+    resultsElement.appendChild(resultitems)
+}
+
+const fromElement = el(
+    "form",
+    {submit: onSubmitHandler},
+    el("label", {}, "Leitarorð:"),
+    inputElement,
+    el("button", {type: "submit"}, "Leita")
+);
+
+artSearcher?.appendChild(fromElement);
+artSearcher?.appendChild(resultsElement);
